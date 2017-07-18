72b4c7a0f30b8801b963c4bd660f85befa3ec29d
MarySalemme <marysalemme1@gmail.com>
2017-07-18 15:15:04 +0100

    Revert "Adding node controller"
    This reverts commit 66970567fb1a9d0facd4a4418182fde8f3f57cbf.

--- notes-app/controllers/note-controller.js
+++
@@ -1,9 +0,0 @@
-window.onclick = function() {
-  var greetings = document.getElementById('app');
-  
-  function changeGreeting(text) {
-    greetings.innerHTML = text;
-  };
-  
-  changeGreeting('Howdy');
-};
